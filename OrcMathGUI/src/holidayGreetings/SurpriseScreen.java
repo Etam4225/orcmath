48d5e564a0609a49526acb58ef58b10a53fb7043
Lenovo <etam4225>
2017-12-21 19:38:40 -0500

    adding fireworks

--- OrcMathGUI/src/holidayGreetings/SurpriseScreen.java
+++ OrcMathGUI/src/holidayGreetings/SurpriseScreen.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.util.List;
 
 import guiTeacher.components.Action;
+import guiTeacher.components.AnimatedComponent;
 import guiTeacher.components.Button;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
@@ -13,12 +14,19 @@ public class SurpriseScreen extends FullFunctionScreen{
 
 	private TextArea area;
 	private Button change;
+	private AnimatedComponent fireworks;
 	
 	public SurpriseScreen(int width, int height) {
 		super(width, height);
 	}
 
 	public void initAllObjects(List<Visible> viewObjects) {
+		fireworks = new AnimatedComponent(250, 265, 29, 34);
+		fireworks.addSequence("resources/mario.png", 150, 234, 50, 29, 34, 3);
+		Thread run = new Thread(fireworks);
+		run.start();
+		viewObjects.add(fireworks);
+		
 		area = new TextArea(250, 250, 200, 200, "Your New Year's resolution should be to go to the gym!");
 		viewObjects.add(area);
 		
