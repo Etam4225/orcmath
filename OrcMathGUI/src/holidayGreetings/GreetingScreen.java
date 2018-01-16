c7a7ad870de750646c12842f5f0f8160fd921044
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2018-01-16 15:21:07 -0500

    struff

--- OrcMathGUI/src/holidayGreetings/GreetingScreen.java
+++ OrcMathGUI/src/holidayGreetings/GreetingScreen.java
@@ -3,6 +3,9 @@ package holidayGreetings;
 import java.awt.Color;
 import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 import guiTeacher.components.*;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
@@ -18,18 +21,23 @@ public class GreetingScreen extends FullFunctionScreen {
 	}
 
 	public void initAllObjects(List<Visible> viewObjects) {
-		StyledComponent.setButtonOutline(true);
+		StyledComponent.setButtonOutline(false);
 		Graphic newyr = new Graphic(0,0,800, 550, "resources/newyear.jpg");
 		viewObjects.add(newyr);
 		Color mCol = new Color(239, 232, 177);
-		change = new Button(200,450,400,50, "Special message brought to you by Arnold Schwarzenegger", mCol, new Action() {
+		change = new Button(200,450,400,50, "   ", new Action() {
 			
 			public void act() {
 				HolidayGUI.holiday.setScreen(HolidayGUI.screen2);
 			}
 		});
+//		change.getActiveBorderColor();
+		change.setActiveBorderColor(null);
 		change.setBackgroundColor(Color.green);
 		viewObjects.add(change);
+		
+		Graphic STUFF = new Graphic(200,450,150, 150, "resources/mario.png");
+		viewObjects.add(STUFF);
 	}
 
 }
